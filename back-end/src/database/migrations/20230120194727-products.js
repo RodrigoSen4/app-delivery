31cf434f5c9c92e3282b814d94cff2be1a4a13a9
Henos <henosvinicius13@gmail.com>
2023-01-25 15:56:23 -0300

    Correções das models e migrations

--- back-end/src/database/migrations/20230120194727-products.js
+++ back-end/src/database/migrations/20230120194727-products.js
@@ -15,7 +15,7 @@ module.exports = {
       },
       price: {
         allowNull: false,
-        type: Sequelize.DECIMAL,
+        type: Sequelize.DECIMAL(4, 2),
       },
       urlImage: {
         allowNull: true,
--- back-end/src/database/models/productModel.js
+++ back-end/src/database/models/productModel.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
       autoIncrement: true,
     },
     name: DataTypes.STRING,
-    price: DataTypes.DECIMAL,
+    price: DataTypes.DECIMAL(4, 2),
     urlImage: DataTypes.STRING,
   },
   {
