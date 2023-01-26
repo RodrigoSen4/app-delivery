73a59bb9f10fa3f656f727db8c71f067a631c605
Henos <henosvinicius13@gmail.com>
2023-01-26 18:31:05 -0300

    Correções no ESLint

--- back-end/src/api/controllers/user.controller.js
+++ back-end/src/api/controllers/user.controller.js
@@ -2,10 +2,10 @@ const md5 = require('md5');
 const generateJWT = require('../auth/generateJWT');
 const { create, getSellers } = require('../services/user.service');
 
-const getAllSellers = async (req, res) => {
+const getAllSellers = async (_req, res) => {
   const sellers = await getSellers();
   return res.status(200).json(sellers);
-}
+};
 
 const createUser = async (req, res) => {
   const { name, email, password } = req.body;
@@ -21,5 +21,5 @@ const createUser = async (req, res) => {
 
 module.exports = {
   createUser,
-  getAllSellers
+  getAllSellers,
 };
--- back-end/src/api/services/sales.service.js
+++ back-end/src/api/services/sales.service.js
@@ -1,20 +1,15 @@
-const { Product, Sale, SaleProduct } = require('../../database/models');
+const { Sale, SaleProduct } = require('../../database/models');
 
 async function createSale(userId, products, saleInfo) {
-  /* let totalPrice = 0;
-  await Promise.all(products.map(async (product) => {
-    const productById = await Product.findOne({ where: { id: product.productId } });
-    const productPrice = productById.price * product.quantity;
-    totalPrice += productPrice;
-  })); */
-  const obj = {
-    userId, ...saleInfo, saleDate: new Date(), status: 'Pendente',
-  };
+  const obj = { userId, ...saleInfo, saleDate: new Date(), status: 'Pendente' };
+
   const newSale = await Sale.create(obj);
+
   await Promise.all(products.map(async (product) => {
     const obj2 = { productId: product.productId, saleId: newSale.id, quantity: product.quantity };
     await SaleProduct.create(obj2);
   }));
+  
   return newSale;
 }
 
